1fe3fe0fd41b6a72109c07ce30441fd4fc7512e5
Casper Bertelsen <casperbj94@gmail.com>
2017-07-28 07:10:09 +0200

    Begin V2

--- SQL - Views/v_log.sql
+++
@@ -1,9 +0,0 @@
-DROP VIEW IF EXISTS greg.v_log_xxxx;
-
-CREATE VIEW greg.v_log_xxxx AS
-
-SELECT 	
-	*
-FROM greg.f_aendring_log (xxxx);
-
-COMMENT ON VIEW greg.v_log_xxxx IS 'Ændringslog, som registrerer alle handlinger indenfor et givent år (xxxx). Benyttes i Ændringslog.xlsx';
